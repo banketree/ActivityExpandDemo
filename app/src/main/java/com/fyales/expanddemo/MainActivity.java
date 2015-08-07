1e4483c00e5b76bffff4004a60b9a702c6409d72
fyales <sww201006@126.com>
2015-08-07 11:43:57 +0800

    first commit

---
+++ app/src/main/java/com/fyales/expanddemo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.fyales.expanddemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.lv1);
+        String[] cs = {"中华人民共和国","美利坚合众国","大不列颠及北爱尔兰联合王国","俄罗斯联邦","法兰西共和国","香港特别行政区","大韩民国"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.item,cs);
+        lv.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
